00a5c141959a529b724394c4854beb331f6deb7b
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2010-07-31 02:56:29 +0900

    Slight change to copylight text.

--- rime.py
+++ rime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2010 TAKAHASHI, Shuhei
+# Copyright (c) 2010 Rime Authors.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +20,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+#
+# Author: Shuhei Takahashi
 #
 
 import sys
